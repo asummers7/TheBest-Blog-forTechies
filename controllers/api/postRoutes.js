1b888afbb9821b0dd6c8416e0aac9ae7274e3b90
Alec S <asummers34@gmail.com>
2023-03-15 22:47:25 -0600

    adding an anchor tag to the commments.

--- controllers/api/postRoutes.js
+++ controllers/api/postRoutes.js
@@ -3,8 +3,8 @@ const withAuth = require('../../utils/auth');
 const {Comment} = require('../../models')
 
 router.post("/", withAuth, async (req, res) => {
-    const comments = await Comment.create({...req.body, user_id: req.session.userId})
-    res.status(200).json(comments);
+    const post = await Comment.create({...req.body, user_id: req.session.userId})
+    res.status(200).json(post);
 });
 
 module.exports = router
